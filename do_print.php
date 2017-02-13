9cee2d59464a118a2b55b1c5ce5736688a785f5f
Juan Jauregui <juan@jauregui.co>
2017-02-12 23:00:02 -0500

    verify_password() causes 500 error And I don't know why :(

--- do_print.php
+++ do_print.php
@@ -1,24 +1,32 @@
 #!/usr/local/bin/php
 
 <?php
-      //Get parameters from POST
-      $submitted_user = $_POST["username"];
-      $submitted_pass = $_POST["password"];
+//Get real path just to be safe
+$real_path = realpath(dirname(__FILE__));
 
-      //Get real path just to be safe
-      $real_path = realpath(dirname(__FILE__));
+//Read the auth file and make it into an associative array, $users_db_arr
+$users_db_str = file_get_contents($real_path."/print_stuff/authorized_users.json");
+$users_db_arr = json_decode($users_db_str,true);
 
-      //Read the auth file and make it into an associative array, $users_db_arr
-      $users_db_str = file_get_contents($real_path."/print_stuff/authorized_users.json");
-      $users_db_arr = json_decode($users_db_str,true);
+$is_auth = FALSE;
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+   $hashed_pw = $users_db_arr[$_POST["username"]];
+   if(isset($hashed_pw)){
+      //printf("password_verify(".$_POST["password"].",".$hashed_pw.")");
+      var_dump($_POST["password"]);
+      var_dump($hashed_pw);
 
-      //var_dump($submitted_pass);
-      //var_dump($users_db_arr[$submitted_user]);
-
-      $is_auth = password_verify($submitted_pass,$users_db_arr[$submitted_user]);
-
-      //If authorized, actually handle the file
-      if($is_auth){
-
+      try{
+         $is_auth = password_verify($_POST["password"],$hashed_pw);
+      } catch (Exception $e) {
+         echo 'Caught exception: ',  $e->getMessage(), "\n";
       }
+   }
+}
+
+if($is_auth){
+   printf("Authorized");
+} else printf("Unauthorized");
+
+
 ?>
--- print_stuff/create_authorized_user.php
+++ print_stuff/create_authorized_user.php
@@ -5,33 +5,46 @@
       //Get real path, as authorized_users.json is relative to script location
       $real_path = realpath(dirname(__FILE__));
 
-      //Will be the index by which users are found
-      $new_user = $argv[1];
-
-      //Hash the password before storing it
-      $new_password = password_hash($argv[2],PASSWORD_BCRYPT);
-
       //Read the auth file and make it into an associative array, $users_db_arr
       $users_db_str = file_get_contents($real_path."/authorized_users.json");
       $users_db_arr = json_decode($users_db_str,true);
 
-      //Add an entry in the array for the new user with the hashed password
-      $users_db_arr[$new_user] = $new_password;
+      //Will be the index by which users are found
+      $new_user = $argv[1];
 
-      //Turn array back into string
-      $users_db_str = json_encode($users_db_arr);
+      if(isset($users_db_arr[$new_user])){
+         if(readline("User already exists. Overwrite? (y/n)") != "y"){
 
-      //Write it back to the file
-      $success = fwrite(fopen($real_path."/authorized_users.json","w"),$users_db_str);
+         }
+      } else {
 
-      if($success){
-         echo "Successfully added user ".$new_user." to the list of authorized users.\n";
+         //Hash the password before storing it
+         printf("Salting and hashing password...\n");
+         $new_password = password_hash($argv[2],PASSWORD_BCRYPT, array('cost' => 14));
+
+         printf("Adding $new_user to the list of authorized users...\n");
+         //Add an entry in the array for the new user with the hashed password
+         $users_db_arr[$new_user] = $new_password;
+
+         //Turn array back into string
+         $users_db_str = json_encode($users_db_arr);
+
+         printf("Saving authorized_users list...\n");
+         //Write it back to the file
+         $success = fwrite(fopen($real_path."/authorized_users.json","w"),$users_db_str);
+
+         if($success){
+            echo "Success!\n";//fully added user ".$new_user." to the list of authorized users.\n";
+         }
+         else {
+            echo "ERROR";
+         }
       }
-      else {
-         echo "Encountered an ERROR";
-      }
+
+
    }
    else
       echo "Usage: php create_authorized_user <new_username> <new_password>\n";
+
 
 ?>
