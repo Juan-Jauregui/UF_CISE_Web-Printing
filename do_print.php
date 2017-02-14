0f9ded3f6322249496bb33e1bffc14283f79ca43
Juan Jauregui <juan@jauregui.co>
2017-02-14 00:43:28 -0500

    Figured out how to SSH Included open-source library phpseclib, which makes it easy to perform SSH. I'm now considering scrapping my user auth scheme and simply passing through the login information to SSH login and letting that either handle the printing or not, depending on whether login succeeds.

--- do_print.php
+++ do_print.php
@@ -9,6 +9,10 @@ $real_path = realpath(dirname(__FILE__));
 /* Include the necessary password-hashing library  */
 include_once($real_path."/print_stuff/passHashLib.php");
 
+/* For SSH */
+set_include_path($real_path.'/print_stuff/phpseclib1.0.5');
+include_once("Net/SSH2.php");
+
 /* Read the auth file and make it into an associative array, $users_db_arr */
 $users_db_str = file_get_contents($real_path."/print_stuff/authorized_users.json");
 $users_db_arr = json_decode($users_db_str,true);
@@ -17,22 +21,25 @@ $hashed_pw = $users_db_arr[$_POST["username"]];
 $is_auth = check_password($_POST["password"],$hashed_pw);
 
 if($is_auth){
-	if(!isset($_POST["not_evil"]))
-	printf("You must be not evil to use this service.\n");
-	else {
-
-		//$target_location = $real_path."/print_stuff/uploads/".$_FILES['upload']['name']
-		//echo "Target location: "$target_location;
+	//var_dump($_FILES);
+	if($_FILES["upload"]["size"] > 0){
 		$temp_location = $_FILES["upload"]["tmp_name"];
 		$final_location = $real_path."/print_stuff/uploads/".$_FILES["upload"]["name"];
 
 		if (move_uploaded_file($temp_location, $final_location)) {
 			echo "File is valid, and was successfully uploaded.\n";
-		} else {
-			echo "Possible file upload attack!\n";
-		}
 
+
+		} else echo "File failed to upload";
+	} else printf("\nNo file uploaded\n");
+
+	$ssh = new Net_SSH2('storm.cise.ufl.edu');
+	if (!$ssh->login('jj3', 'nicetry')) {
+		 exit('Login Failed');
 	}
+
+	echo $ssh->exec('lpstat -p -d');
+
 } else printf("\nUnauthorized");
 
 
--- print.php
+++ print.php
@@ -2,53 +2,64 @@
 
 <html>
 <head>
-   <title>PHP Test</title>
-   <!-- Bootstrap-->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Web Print</title>
+	<!-- Bootstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-   <div class="container">
-      <div class="row">
-         <div class="col-sm-6 col-sm-offset-3">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4 col-xs-offset-4">
+				<div class="page-header">
+					<h1>Print Files</h1>
+				</div>
+			</div>
+		</div>
 
-            <div class="page-header">
-               <h1>Print Files</h1>
-            </div>
+		<div class="row">
 
-            <form action="./do_print.php" method="post" enctype="multipart/form-data">
-               <div class="form-group">
-                  <label for="username_input">CISE Login</label>
-                  <input name="username" type="username" class="form-control" id="username_input" placeholder="Username">
-               </div>
+			<div class="col-xs-6 col-xs-offset-3">
+				<form action="./do_print.php" method="post" enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="username_input">Login</label>
+						<input name="username" type="username" class="form-control" id="username_input" placeholder="Username">
+					</div>
 
-               <div class="form-group">
-                  <label for="password_input">Password</label>
-                  <input name="password" type="password" class="form-control" id="password_input" placeholder="Password">
-               </div>
+					<div class="form-group">
+						<label for="password_input">Password</label>
+						<input name="password" type="password" class="form-control" id="password_input" placeholder="Password">
+					</div>
 
-               <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input name="upload" type="file" id="input_file">
-               </div>
+					<div class="row">
+						<div class="col-xs-4">
+							<div class="form-group">
+								<label for="printer">Printer</label><br />
+								<input name="printer" type="radio" id="ps114"> ps114
+								<br />
+								<input name="printer" type="radio" id="ps309"> ps309
+							</div>
+						</div>
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label for="exampleInputFile">File input</label>
+								<input name="upload" type="file" id="input_file">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<button type="submit" class="btn btn-block btn-primary">Submit</button>
+					</div>
+				</form>
+			</div>
 
-               <div class="checkbox">
-                  <label>
-                     <input name="not_evil" type="checkbox" id="not_evil"> I'm not evil
-                  </label>
-               </div>
-
-               <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-         </div>
-
-      </div>
-   </div>
+		</div>
+	</div>
 
 
-   <!-- Bootstrap JavaScript -->
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 
 </html>
